844f42b43334f24d3e0e8525c19e884d9a93d8ac
Sapan Tiwari <sapantiwari@sapans-mbp.lan>
2018-05-27 16:11:42 -0700

    Output works, added styles

--- robot.js
+++ robot.js
@@ -1,58 +1,70 @@
 var robot = document.getElementById('robot');
-var buttonLeft = document.getElementById('left');
+var buttonLeftPosition = document.getElementById('left');
 var buttonRight = document.getElementById('right');
 var buttonMove = document.getElementById('move');
 var buttonReport = document.getElementById('report');
 var output = document.getElementById('output');
-var direction;
-var top1 = 0, left = 0;
+var direction, errorText;
+var topPosition = 0, leftPosition = 0;
 
 
 function intialPosition() {
-	top1 = 25; left = 25;
+	topPosition = 100; leftPosition = 25;
 	direction = "EAST";
-	robot.style.top = top1+"px";
-	robot.style.left = left+"px";
+	robot.style.top = topPosition+"px";
+	robot.style.left = leftPosition+"px";
 	robot.innerHTML = direction;
 }
 function updateOutput() {
-	output.innerHTML = Math.floor(left%100)+","+Math.floor(top1%100)+","+direction;
+	output.innerHTML = Math.floor(leftPosition/100)+", "+Math.floor((topPosition-1)/100)+", "+direction;
+}
+function outputError() {
+	output.innerHTML = "Please change direction...";
 }
 document.addEventListener('DOMContentLoaded', function(e) {
 	intialPosition();
 	document.addEventListener('click', function() {
 		updateOutput();
 	});
-	buttonMove.addEventListener('click', function() {
+	buttonMove.addEventListener('click', function(e) {
 		if(direction === "EAST") {
-			if(left < 425) {
-				left += 100;
-				console.log(left);
-				robot.style.left = left+"px";
+			if(leftPosition < 425) {
+				leftPosition += 100;
+				robot.style.left = leftPosition+"px";
 			} 
 			else {
-				alert("Please change the direction");
+				outputError();
+				e.stopPropagation();
 			}
 		}
 		else if(direction === "SOUTH") {
-			if(top1 < 425) {
-				top1 += 100;
-				console.log(top1);
-				robot.style.top = top1+"px";
+			if(topPosition < 425) {
+				topPosition += 100;
+				robot.style.top = topPosition+"px";
+			}
+			else {
+				outputError();
+				e.stopPropagation();
 			}
 		}
 		else if(direction === "WEST") {
-			if(left > 100) {
-				left -= 100;
-				console.log(left);
-				robot.style.left = left+"px";
+			if(leftPosition > 100) {
+				leftPosition -= 100;
+				robot.style.left = leftPosition+"px";
+			}
+			else {
+				outputError();
+				e.stopPropagation();
 			}
 		}
 		else if(direction === "NORTH") {
-			if(top1 > 100) {
-				top1 -= 100;
-				console.log(top1);
-				robot.style.top = top1+"px";
+			if(topPosition > 100) {
+				topPosition -= 100;
+				robot.style.top = topPosition+"px";
+			}
+			else {
+				outputError();
+				e.stopPropagation();
 			}
 		}
 	});
@@ -65,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 		robot.innerHTML = direction;
 	});
 
-	buttonLeft.addEventListener('click', function() {
+	buttonLeftPosition.addEventListener('click', function() {
 		if(direction === "EAST") direction = "NORTH";
 		else if(direction === "NORTH") direction = "WEST";
 		else if(direction === "WEST") direction = "SOUTH";
